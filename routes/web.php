fa1b274f6587bf3f6b60899fcf138c442696a3ca
mahmudul-elahi <mahmudul.elahi@gmail.com>
2026-03-08 08:45:39 +0600

    dsfa

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@ Route::get('clear', function () {
 });
 
 Route::post('stripe/webhook', [WebhookController::class, 'handleWebhook']);
+
+
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+});
