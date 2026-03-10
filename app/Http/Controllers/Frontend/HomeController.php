d41d41d8047c0b3b843d90b404e32d6107109e8a
mahmudul-elahi <mahmudul.elahi@gmail.com>
2026-03-10 12:32:52 +0600

    add pagination on news and gallary

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -7,19 +7,22 @@ use App\Http\Resources\GalleryResource;
 use App\Http\Resources\NewsResource;
 use App\Models\Gallery;
 use App\Models\News;
+use Illuminate\Support\Facades\Request;
 
 class HomeController extends Controller
 {
-    public function get_featured_news()
+    public function get_featured_news(Request $request)
     {
-        $featured_posts = News::published()->featured()->latest()->get();
+        $per_page = $request->query('per_page', 5);
+        $featured_posts = News::latest()->paginate($per_page);
         return $this->sendResponse(NewsResource::collection($featured_posts));
     }
 
 
-    public function get_featured_gallery()
+    public function get_featured_gallery(Request $request)
     {
-        $featured_gallery = Gallery::featured()->latest()->get();
+        $per_page = $request->query('per_page', 5);
+        $featured_gallery = Gallery::latest()->paginate($per_page);
         return $this->sendResponse(GalleryResource::collection($featured_gallery));
     }
 }
