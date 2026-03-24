2d8313f05b41f8fd602a29293ddd81d662e0df24
Shahadat-Hossain-Shanto <shahadathossain1563@gmail.com>
2026-03-24 15:33:19 +0600

    match for voting

---
+++ app/Models/MatchForVoting.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MatchForVoting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'game_id',
+        'player_one_id',
+        'player_two_id',
+        'total_vote',
+    ];
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function playerOne()
+    {
+        return $this->belongsTo(User::class, 'player_one_id');
+    }
+
+    public function playerTwo()
+    {
+        return $this->belongsTo(User::class, 'player_two_id');
+    }
+}
