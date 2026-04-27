c256d7dd58dafaf4a0f0aa233d37ac0b71cad57a
mahmudul-elahi <mahmudul.elahi@gmail.com>
2026-04-27 08:49:53 +0600

    migrationfile and route

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\Bitpay\BitpayConnectController;
+use App\Http\Controllers\Bitpay\BitpayWithdrawController;
 use App\Http\Controllers\Frontend\FollowController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\PostController;
 use App\Http\Controllers\Frontend\ProfileController;
 use App\Http\Controllers\Frontend\TwitchController;
+use App\Http\Controllers\Moncash\MoncashConnectController;
+use App\Http\Controllers\Moncash\MoncashWithdrawController;
+use App\Http\Controllers\Paypal\PaypalConnectController;
+use App\Http\Controllers\Paypal\PaypalWithdrawController;
 use App\Http\Controllers\Stripe\CheckoutController;
 use App\Http\Controllers\Stripe\StripeConnectController;
 use App\Http\Controllers\Stripe\StripeWithdrawController;
@@ -24,11 +30,31 @@ Route::middleware(['auth:api'])->group(function () {
     // Twitch live
     Route::get('twitch/check_live', [TwitchController::class, 'status']);
 
-    // Payment Releted
+    // Payment
     Route::post('checkout', [CheckoutController::class, 'checkout']);
     Route::post('connect_account', [CheckoutController::class, 'connect_account']);
+
+    // Stripe payout account
     Route::post('stripe/connect', [StripeConnectController::class, 'connect']);
     Route::get('stripe/status', [StripeConnectController::class, 'status']);
+    Route::post('stripe/withdraw', [StripeWithdrawController::class, 'request']);
+
+    // PayPal payout account
+    Route::post('paypal/connect', [PaypalConnectController::class, 'connect']);
+    Route::get('paypal/status', [PaypalConnectController::class, 'status']);
+    Route::post('paypal/withdraw', [PaypalWithdrawController::class, 'request']);
+
+    // BitPay payout account
+    Route::post('bitpay/connect', [BitpayConnectController::class, 'connect']);
+    Route::get('bitpay/status', [BitpayConnectController::class, 'status']);
+    Route::post('bitpay/withdraw', [BitpayWithdrawController::class, 'request']);
+
+    // MonCash payout account
+    Route::post('moncash/connect', [MoncashConnectController::class, 'connect']);
+    Route::get('moncash/status', [MoncashConnectController::class, 'status']);
+    Route::post('moncash/withdraw', [MoncashWithdrawController::class, 'request']);
+
+    // Legacy withdraw route (Stripe)
     Route::post('withdraw/request', [StripeWithdrawController::class, 'request']);
 
     Route::get('show_artist_prifile/{id}', [ProfileController::class, 'show_artist_prifile']);
