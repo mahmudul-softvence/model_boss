c2f6450eb1658199a5f6da770cc057a47b607467
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-06-18 18:13:19 +0600

    added: notifications for

--- app/Notifications/ChallengeOfferNotification.php
+++ app/Notifications/ChallengeOfferNotification.php
@@ -6,6 +6,7 @@ use App\Models\Challenge;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class ChallengeOfferNotification extends Notification implements ShouldQueue
@@ -14,9 +15,24 @@ class ChallengeOfferNotification extends Notification implements ShouldQueue
 
     public function __construct(protected Challenge $challenge) {}
 
+    /**
+     * @return array<int, string>
+     */
     public function via(object $notifiable): array
     {
-        return ['database', 'broadcast'];
+        return ['mail', 'database', 'broadcast'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('You have been challenged on Model Boss')
+            ->view('emails.challenge-offer', [
+                'notifiable_name' => $notifiable->name,
+                'challenger_name' => $this->challenge->challenger?->name,
+                'challenge_no' => $this->challenge->challenge_no,
+                'amount' => $this->challenge->amount,
+            ]);
     }
 
     public function toDatabase(object $notifiable): array
